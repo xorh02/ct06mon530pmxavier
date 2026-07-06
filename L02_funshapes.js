6b8746ec384730056579adee82f6f59ef679e216
xorh02 <xavierong02@gmail.com>
2026-07-06 10:08:27 +0000

    Jul 6, 2026, 10:08 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -22,7 +22,7 @@ function draw() {
   //circle(x,y,diameter)
 
   circle(300,200,200);
-  circle(200,100,50)
+  circle(200,100,75);
   // Challenge 3: A house using rect() function
 
   // Challenge 4: A rocketship using triangle() function
