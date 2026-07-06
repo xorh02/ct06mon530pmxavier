aa21ab1f8e52514580b23719601a3659ef2c8a87
xorh02 <xavierong02@gmail.com>
2026-07-06 10:45:20 +0000

    Jul 6, 2026, 10:45 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -38,7 +38,7 @@ function draw() {
   // rect(250,300,100,150);
   // ellipse(320,350,20,50);
   // Challenge 4: A rocketship using triangle() function
-  fill ()
+  fill (255,20,0)
   triangle(100,200,250,230,10,10);
   // Challenge 5: A square face using quad() function
 
