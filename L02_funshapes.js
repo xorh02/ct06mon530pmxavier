0b970d43117e4aeae790efc9a2bdeead88cd12eb
xorh02 <xavierong02@gmail.com>
2026-07-06 10:44:05 +0000

    Jul 6, 2026, 10:44 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -44,5 +44,6 @@ function draw() {
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
-  fill (0,0,0)
+  fill (0,0,0) //black color
+  fill (255,255,0)//yellow color
 }
