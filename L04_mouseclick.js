9b4c10028274d978713ded82807aef71d258eb9e
xorh02 <xavierong02@gmail.com>
2026-07-20 10:18:00 +0000

    Jul 20, 2026, 10:18 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -8,6 +8,7 @@ function setup(){
 function draw(){
     for(let i=0; i <5;i++){
       //fill(0) = black fill(255) = white
+      //fill(red,green ,blue) 
         fill(0,0 + i*25,0);
       //circle (x position , y position , diameter);
         circle(30 + i*30 ,30+i*30, 30);
