564f6c7676860fe46fb043a28ea8a4d523d65fdb
xorh02 <xavierong02@gmail.com>
2026-07-20 10:23:19 +0000

    Jul 20, 2026, 10:23 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -6,14 +6,14 @@ function setup(){
   background(220);
 }
 function draw(){
-    for(let i=0; i <5;i++){
+  
       //fill(0) = black fill(255) = white
       //fill(red,green ,blue) -> number between 0 and 255
         fill(shapeColor);
       //circle (x position , y position , diameter);
         circle(mouseX ,mouseY, 30);
         //mouseX is the x position of my mouse, mouseY is the y position of my mouse
-    }
+    
     //mousePressed()
     //mouseReleased()
 }
