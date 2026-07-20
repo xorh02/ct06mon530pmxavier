c917460198a97afd4917a3596296f441e714cac8
xorh02 <xavierong02@gmail.com>
2026-07-20 09:57:54 +0000

    Jul 20, 2026, 9:57 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -24,12 +24,15 @@ function draw() {
   let diameter = 30;
   let numCircle = 5;
   let totalwidth = numCircle * diameter;
-  let startY = 
+  let startY = diameter/2;
   let startX =  diameter/2;
+
+  for(let i=0;i<numCircle;i++){
   for(let i=0; i<numCircle; i++){
     let x = startX + i * diameter;
     ellipse(x,15,diameter,diameter);
   }
+}
   // Task 4: Grid of Circles
 
 }
