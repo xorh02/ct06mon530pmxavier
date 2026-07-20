f6da7fd038ea16e86851829ca1027bc080182fde
xorh02 <xavierong02@gmail.com>
2026-07-20 10:40:55 +0000

    Jul 20, 2026, 10:40 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -19,7 +19,7 @@ function draw(){
 
     //Task 1: Random Color Preview
     fill(shapeColor)
-    circle(200,200,300);
+    // circle(200,200,300);
     triangle(random(0,400),random(0,400),random(0,400),random(0,400),random(0,400),random(0,400)  );
 }
 function mousePressed(){
