65e5dfa1ac9054847a6935317745e92989a05278
xorh02 <xavierong02@gmail.com>
2026-07-22 08:49:24 +0000

    Jul 22, 2026, 8:49 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -35,7 +35,7 @@ function draw() {
   let startX =  (width - totalwidth)/2+circlediamter/2;
   let startY = (height-totalwidth)/2 +circlediamter/2;
   for(let row = 0;row<numofcircle;row++){
-    let y = row
+    let y = startY + (row*circlediamter);
     for(let i =0;i<numofcircle;i++){
       if(i%2===0){
         fill(255);
