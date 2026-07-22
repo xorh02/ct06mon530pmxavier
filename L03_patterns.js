e0d22ccb780e4f2b7583403b7b076d089cb31f3a
xorh02 <xavierong02@gmail.com>
2026-07-22 08:00:26 +0000

    Jul 22, 2026, 8:00 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -8,7 +8,8 @@ function draw() {
   // Recap 1: Repeating Circles
   
   // Task 1: Colour Gradient    
-  fill()
+  fill(23);
+  circle(100,100,20);
   // Task 2: Colour Loop
 
   // Task 3: Row of Circles
