57536e525b2b415a1bff7308adbd3f6b8d5c0e9d
xorh02 <xavierong02@gmail.com>
2026-07-22 07:25:26 +0000

    Jul 22, 2026, 7:25 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,43 +1,5 @@
 // write your codes here
-let shapeColor = 'white';
-let x1,x2,x3,y1,y2,y3;
+
 
 //type in the two basic function
-function setup(){
-  createCanvas(400,400);
-  background(220);
-}
-function draw(){
-    // for(let i=0; i <5;i++){
-    //   //fill(0) = black fill(255) = white
-    //   //fill(red,green ,blue) -> number between 0 and 255
-    //     fill(shapeColor);
-    //   //circle (x position , y position , diameter);
-    //     circle(mouseX ,mouseY, 30);
-    // //mouseX is the x position of my mouse, mouseY is the y position of my mouse
-    // }
-    //mousePressed()
-    //mouseReleased()
 
-    //Task 1: Random Color Preview
-    fill(shapeColor)
-    // circle(200,200,300);
-    triangle(x1,y1,x2,y2,x3,y3 );
-    
-}
-function mouseMoved(){
-    ellipse(mouseX,mouseY,20,10);
-}
-function mousePressed(){
-    shapeColor = color(random(255),random(255),random(255));
-    x1 = random(400);
-    y1 = random(400);
-    x2 = random(400);
-    y2 = random(400);
-    x3 = random(400);
-    y3 = random(400);
-}
-function mouseReleased(){
-    shapeColor = 'white';
- 
-}
