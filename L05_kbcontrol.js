0daad7faf8df54203ebbc75c001c5d30ceefdaf4
xorh02 <xavierong02@gmail.com>
2026-07-27 10:24:36 +0000

    Jul 27, 2026, 10:24 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,12 +1,11 @@
 // write your codes here
-let rectsize =  50;
+
 function setup(){
     createCanvas(800,400);
-
 }
 function draw(){
     background(220);
-    rect(width/2,height/2,rectsize,rectsize);
+   
 }
 function keyPressed(){
     let num = random(5,20);
