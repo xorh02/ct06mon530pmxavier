b83cf2a725f068bc6806f3173348242dcec61992
xorh02 <xavierong02@gmail.com>
2026-07-27 10:25:16 +0000

    Jul 27, 2026, 10:25 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,11 +1,13 @@
 // write your codes here
-
+let showcircle = false;
+let showtriangle = false;
+let showsquare = false;
 function setup(){
     createCanvas(800,400);
 }
 function draw(){
     background(220);
-    
+    if(showcircle)
 }
 function keyPressed(){
    
