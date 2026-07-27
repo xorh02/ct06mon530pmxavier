ad325c5a0c071734462c4c98ef91aa5c0ddb1113
xorh02 <xavierong02@gmail.com>
2026-07-27 10:01:10 +0000

    Jul 27, 2026, 10:01 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -12,7 +12,7 @@ function keyPressed(){
     let num = random(5,20);
     let size = random(5,20);
     for(let i =0;i<num;i++){
-        circle()
+        circle(random(800),random(400),)
     }
 }
 function keyReleased(){
