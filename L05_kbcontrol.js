166c63f402c4836af1b9d109fb6bdf8976689dbe
xorh02 <xavierong02@gmail.com>
2026-07-27 09:51:59 +0000

    Jul 27, 2026, 9:51 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -2,9 +2,10 @@
 let rectsize =  50;
 function setup(){
     createCanvas(800,400);
-    background(220);
+
 }
 function draw(){
+        background(220);
     rect(width/2,height/2,rectsize,rectsize);
 }
 function keyPressed(){
