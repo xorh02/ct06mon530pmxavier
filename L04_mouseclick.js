fbe8f46073d5c04ce0c8cd12f220e0c38a16d5f9
xorh02 <xavierong02@gmail.com>
2026-07-27 09:43:19 +0000

    Jul 27, 2026, 9:43 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -14,7 +14,7 @@ function mouseDragged(){
     //challenge 3
     let size = random(5,20);
     fill(random(255),random(255),random(255));
-    //random(-20,20)
+    //random(-20,20)-> generate a number between -20 and 20
     circle(mouseX +random(-20,20),mouseY + random(-20,20),size);
 }
 function mouseReleased(){
