70d3e02c6253f612d01951b8d662ac4988f7d85a
xorh02 <xavierong02@gmail.com>
2026-08-17 10:54:36 +0000

    Aug 17, 2026, 10:54 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -41,7 +41,7 @@ function draw(){
         staticSize -=1;
         staticSize >300? staticSize=300:staticSize<0?staticSize =0:StaticSize
     }
-    if(staticImageX <0 || staticImageX>400){}
+    if(staticImageX <0 || staticImageX>400 || staticImageY<0||staticImageY){}
     
 }
 function keyPressed(){
