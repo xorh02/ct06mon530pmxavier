be0512169a9d55405ac69e0e1e305c1c80627fbd
xorh02 <xavierong02@gmail.com>
2026-08-17 09:41:56 +0000

    Aug 17, 2026, 9:41 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,9 +1,11 @@
 // write your codes here
+let x = 200;
+let  y= 200;
 function setup(){
     createcanvas(400,400);
     background(220);
 }
 
 function draw(){
-    square(200,200)
+    square(x,200)
 }
