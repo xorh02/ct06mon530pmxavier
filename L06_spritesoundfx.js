1afff79aae2ee6f549eaabd62fe3b431f174022d
xorh02 <xavierong02@gmail.com>
2026-08-17 10:52:56 +0000

    Aug 17, 2026, 10:52 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -5,6 +5,7 @@ let staticImageX = 0;
 let staticImageY = 0;
 let staticSpeed = 5;
 let staticSize =50;
+let colorr = color()
 function preload(){
     soundEffect = loadSound('assets/pop.mp3');
     bgMusic = loadSound('assets/bossaNova.mp3');
