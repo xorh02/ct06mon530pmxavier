3f4fb590d8f2c8c79a5f87c650e15903d4f1ec6c
xorh02 <xavierong02@gmail.com>
2026-08-17 09:49:55 +0000

    Aug 17, 2026, 9:49 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,5 +1,10 @@
 // write your codes here
-let soundEffect ,bgMusic
+let soundEffect ,bgMusic, staticImage;
+function preload(){
+    
+
+}
+
 function setup(){
     createCanvas(400,400);
     background(220);
