5dd5bed9a685d22345fe7f9e3da59870d64fc3db
xorh02 <xavierong02@gmail.com>
2026-08-17 10:33:09 +0000

    Aug 17, 2026, 10:33 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -17,7 +17,7 @@ function setup(){
 }
 function draw(){
    background("lightblue");
-   image(staticImage,staticImageX,staticImageY,110,133);
+   image(staticImage,staticImageX,staticImageY,50,50);
 }
 function keyPressed(){
     if(keyCode === 32){
