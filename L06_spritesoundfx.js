15d6123f1a71dc6c517807924e848b097c72962c
xorh02 <xavierong02@gmail.com>
2026-08-17 09:49:23 +0000

    Aug 17, 2026, 9:49 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,22 +1,10 @@
 // write your codes here
-let x = 200;
-let y = 200;
+
 function setup(){
     createCanvas(400,400);
     background(220);
 }
 
 function draw(){
-    rect(x,y,25,25);
-    if(keyIsDown(UP_ARROW)){
-        y -=2;
-    }else if(keyIsDown(DOWN_ARROW)){
-        y +=2;
-    }else if(keyIsDown(LEFT_ARROW) ){
-        x -=2;
-    }else if(keyIsDown(RIGHT_ARROW)){
-        x +=2;
-    }
-    constrain(x,50,300);
-    constrain(y,50,300);
+   
 }
