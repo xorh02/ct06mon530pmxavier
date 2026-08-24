fd641c2831158cd533cb2944b7f971f69ac32d77
xorh02 <xavierong02@gmail.com>
2026-08-24 10:39:12 +0000

    Aug 24, 2026, 10:39 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -35,51 +35,51 @@
 
 
 //task1 moving balls
-let ballx = 300;
-let bally = 200;
-let ballsize = 30;
-let ballspeedx = 3;
-let ballspeedy = 3;
-let colorchange = false; let trail = false; let r=0; let g=0; let b=255;
-function setup(){
-    createCanvas(600,400);
-    background(220);
-}
-function draw(){
-    noStroke();
-    if(!trail){background(220);}
-    circle(ballx,bally,ballsize);
-    ballx += ballspeedx;
-    bally += ballspeedy;
-    //task 2 condition for edge detection
-    //first if for the left and right
-    if(ballx + (ballsize/2) >= width || ballx - (ballsize/2)<=0){
-        ballspeedx = -ballspeedx;
-        if(colorchange){
-        r = random(255);g = random(255);   b = random(255);
-    }
-    fill(r,g,b);
-    }
-    //second iff for the top and the bottom
-    if(bally + (ballsize/2) > height || bally < ballsize/2){
-        ballspeedy = -ballspeedy;
-       if(colorchange){
-        r = random(255); g = random(255);b = random(255);
-    }
-    fill(r,g,b);
-    }
-}
-function keyPressed(){
-    //task 4 speed control
-    if(keyCode ===UP_ARROW){ ballspeedx *= 1.2; ballspeedy *=1.2;} 
-    else if(keyCode ===DOWN_ARROW){ ballspeedx *= 0.8; ballspeedy *=0.8;}
-    if(key === 'c' || key === 'C'){ colorchange = !colorchange;}
-    if(key === 't' || key === 'T'){ trail = !trail;}
-    if(key === '+'){ ballsize +=5;}
-    if(key === '-'){ ballsize -=5;}
-}
+// let ballx = 300;
+// let bally = 200;
+// let ballsize = 30;
+// let ballspeedx = 3;
+// let ballspeedy = 3;
+// let colorchange = false; let trail = false; let r=0; let g=0; let b=255;
+// function setup(){
+//     createCanvas(600,400);
+//     background(220);
+// }
+// function draw(){
+//     noStroke();
+//     if(!trail){background(220);}
+//     circle(ballx,bally,ballsize);
+//     ballx += ballspeedx;
+//     bally += ballspeedy;
+//     //task 2 condition for edge detection
+//     //first if for the left and right
+//     if(ballx + (ballsize/2) >= width || ballx - (ballsize/2)<=0){
+//         ballspeedx = -ballspeedx;
+//         if(colorchange){
+//         r = random(255);g = random(255);   b = random(255);
+//     }
+//     fill(r,g,b);
+//     }
+//     //second iff for the top and the bottom
+//     if(bally + (ballsize/2) > height || bally < ballsize/2){
+//         ballspeedy = -ballspeedy;
+//        if(colorchange){
+//         r = random(255); g = random(255);b = random(255);
+//     }
+//     fill(r,g,b);
+//     }
+// }
+// function keyPressed(){
+//     //task 4 speed control
+//     if(keyCode ===UP_ARROW){ ballspeedx *= 1.2; ballspeedy *=1.2;} 
+//     else if(keyCode ===DOWN_ARROW){ ballspeedx *= 0.8; ballspeedy *=0.8;}
+//     if(key === 'c' || key === 'C'){ colorchange = !colorchange;}
+//     if(key === 't' || key === 'T'){ trail = !trail;}
+//     if(key === '+'){ ballsize +=5;}
+//     if(key === '-'){ ballsize -=5;}
+// }
 
-
+function setup(){}
 
 
 
