4138d05247b0ee061f1be0d185a9de7800e0b03a
xorh02 <xavierong02@gmail.com>
2026-08-24 11:01:47 +0000

    .

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,12 +1,12 @@
 // write your codes here
-// let pico , popSound;
+let pico , popSound;
 // let x = 200;
 // let y = 200;
 // let picowidth = 100;
-// function preload(){
-//     pico = loadImage('assets/pico-a.png');
-//     popSound = loadSound('assets/pop.mp3');
-// }
+function preload(){
+    pico = loadImage('assets/pico-a.png');
+    popSound = loadSound('assets/bossaNova.mp3');
+}
 // function setup(){
 //     createCanvas(600,400);
 // }
@@ -34,63 +34,82 @@
 // }
 
 
-//task1 moving balls
-// let ballx = 300;
-// let bally = 200;
-// let ballsize = 30;
-// let ballspeedx = 3;
-// let ballspeedy = 3;
-// let colorchange = false; let trail = false; let r=0; let g=0; let b=255;
+let ballx = 300;
+let bally = 200;
+let ballsize = 30;
+let ballspeedx = 3;
+let ballspeedy = 3;
+let colorchange = false; let trail = false; let r=0; let g=0; let b=255;
+function setup(){
+    createCanvas(600,400);
+    popSound.play();
+    background(0);
+}
+function draw(){
+    noStroke();
+    if(!trail){background(220);}
+    circle(ballx,bally,ballsize);
+    ballx += ballspeedx;
+    bally += ballspeedy;
+    //task 2 condition for edge detection
+    //first if for the left and right
+    if(ballx + (ballsize/2) >= width || ballx - (ballsize/2)<=0){
+        ballspeedx = -ballspeedx;
+        if(colorchange){
+        r = random(255);g = random(255);   b = random(255);
+    }
+    fill(r,g,b);
+    }
+    //second iff for the top and the bottom
+    if(bally + (ballsize/2) > height || bally < ballsize/2){
+        ballspeedy = -ballspeedy;
+       if(colorchange){
+        r = random(255); g = random(255);b = random(255);
+    }
+    fill(r,g,b);
+    }
+}
+function keyPressed(){
+    //task 4 speed control
+    if(keyCode ===UP_ARROW){ ballspeedx *= 1.2; ballspeedy *=1.2;} 
+    else if(keyCode ===DOWN_ARROW){ ballspeedx *= 0.8; ballspeedy *=0.8;}
+    if(key === 'c' || key === 'C'){ colorchange = !colorchange;}
+    if(key === 't' || key === 'T'){ trail = !trail;}
+    if(key === '+'){ ballsize +=5;}
+    if(key === '-'){ ballsize -=5;}
+}
+
 // function setup(){
 //     createCanvas(600,400);
+//     background(0);
+// }
+// function draw(){
+//     stroke("red");
+//     fill("black");
+//     strokeWeight(10);
+//     circle (150,200,80);
+
+// }
+
+// let x = 50;
+// let y=50;
+// function setup(){
+//     createCanvas(600,600);
 //     background(220);
 // }
 // function draw(){
-//     noStroke();
-//     if(!trail){background(220);}
-//     circle(ballx,bally,ballsize);
-//     ballx += ballspeedx;
-//     bally += ballspeedy;
-//     //task 2 condition for edge detection
-//     //first if for the left and right
-//     if(ballx + (ballsize/2) >= width || ballx - (ballsize/2)<=0){
-//         ballspeedx = -ballspeedx;
-//         if(colorchange){
-//         r = random(255);g = random(255);   b = random(255);
-//     }
-//     fill(r,g,b);
-//     }
-//     //second iff for the top and the bottom
-//     if(bally + (ballsize/2) > height || bally < ballsize/2){
-//         ballspeedy = -ballspeedy;
-//        if(colorchange){
-//         r = random(255); g = random(255);b = random(255);
-//     }
-//     fill(r,g,b);
+//     for(let i =0; i<10;i++){
+//         let color = i * 50;
+//         fill(color);
+//         circle(x + i*50,y + i*50,50);
 //     }
 // }
-// function keyPressed(){
-//     //task 4 speed control
-//     if(keyCode ===UP_ARROW){ ballspeedx *= 1.2; ballspeedy *=1.2;} 
-//     else if(keyCode ===DOWN_ARROW){ ballspeedx *= 0.8; ballspeedy *=0.8;}
-//     if(key === 'c' || key === 'C'){ colorchange = !colorchange;}
-//     if(key === 't' || key === 'T'){ trail = !trail;}
-//     if(key === '+'){ ballsize +=5;}
-//     if(key === '-'){ ballsize -=5;}
-// }
-let x = 50;
-let y=50;
-function setup(){
-    createCanvas(600,600);
-    background(220);
-}
-function draw(){
-    for(let i =0; i<3;)
-        circle(x,y,50);
-}
-
-
-
+// heading in html
+// <h1> to h7
+// declare variable let smt = smt;
+// for loop syntax for(let i=0; i<9; i++){action}
+// createCanvas()
+// css stands for cascading style sheets 
 
 
 
