63470493a3634018975dac2c61945909c0015f8c
xorh02 <xavierong02@gmail.com>
2026-08-24 10:20:49 +0000

    Aug 24, 2026, 10:20 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -66,8 +66,8 @@ function draw(){
 function keyPressed(){
     //task 4 speed control
     if(keyCode ===UP_ARROW){ ballspeedx *= 1.2; ballspeedy *=1.2;} 
-}   if(keyCode ===DOWN_ARROW){ ballspeedx *= 0.8; ballspeedy *=0.8;}
-
+  else if(keyCode ===DOWN_ARROW){ ballspeedx *= 0.8; ballspeedy *=0.8;}
+}
 
 
 
