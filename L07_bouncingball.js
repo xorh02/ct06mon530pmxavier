e26f4fc54236c1f43728aa5bb9b359c0f2e854a2
xorh02 <xavierong02@gmail.com>
2026-08-24 09:51:12 +0000

    Aug 24, 2026, 9:51 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,38 +1,56 @@
 // write your codes here
-let pico , popSound;
-let x = 200;
-let y = 200;
-let picowidth = 100;
-function preload(){
-    pico = loadImage('assets/pico-a.png');
-    popSound = loadSound('assets/pop.mp3');
-}
-function setup(){
-    createCanvas(600,400);
-}
-function draw(){
-    background(220);
-    if(keyIsDown(LEFT_ARROW)){
-        x -=3;
-    }
-    if(keyIsDown(RIGHT_ARROW)){
-        x+=3;
-    }
-    if(keyIsDown(UP_ARROW)){
-        y-=3;
-    }
-    if(keyIsDown(DOWN_ARROW)){
-        y+=3;
-    }
-    image(pico,x,y,picowidth,100);
-}
-function keyPressed(){
-    if(key===  " "){
-        picowidth /=2;
-        popSound.play ();
-        if(picowidth <=0){
-            picowidth = 1;
-            picowidth *=2;
-        }
-    }
-}
+// let pico , popSound;
+// let x = 200;
+// let y = 200;
+// let picowidth = 100;
+// function preload(){
+//     pico = loadImage('assets/pico-a.png');
+//     popSound = loadSound('assets/pop.mp3');
+// }
+// function setup(){
+//     createCanvas(600,400);
+// }
+// function draw(){
+//     background(220);
+//     if(keyIsDown(LEFT_ARROW)){
+//         x -=3;
+//     }
+//     if(keyIsDown(RIGHT_ARROW)){
+//         x+=3;
+//     }
+//     if(keyIsDown(UP_ARROW)){
+//         y-=3;
+//     }
+//     if(keyIsDown(DOWN_ARROW)){
+//         y+=3;
+//     }
+//     image(pico,x,y,picowidth,100);
+// }
+// function keyPressed(){
+//     if(key===  " "){
+//         picowidth /=2;
+//         popSound.play ();
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
