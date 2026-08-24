9988f44c7377469ea71bc92420d1f5402612eef8
xorh02 <xavierong02@gmail.com>
2026-08-24 10:29:02 +0000

    Aug 24, 2026, 10:29 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -42,7 +42,8 @@ let ballspeedx = 3;
 let ballspeedy = 3;
 let colorchange = false; let trail = false; let r=0; let g=0; let b=255;
 function setup(){
-    createCanvas(600,400);background(220);
+    createCanvas(600,400);
+    background(220);
 }
 function draw(){
     noStroke();
