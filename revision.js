db9f5d4aa4c704414fa38c269ce2895db860792c
xorh02 <xavierong02@gmail.com>
2026-08-30 09:21:01 +0000

    Aug 30, 2026, 9:21 AM

--- revision.js
+++ revision.js
@@ -35,7 +35,8 @@ heading in html -> h1 to h7
 must have in html -> body, title ,html ,head
 declare a variable -> let variable_name = value;
 must have functions in p5js -> setup() and draw()
-in for loop be very careful of the initial v
+in for loop be very careful of the initial variable declaration
+
 
 
 */
