41c4358238f1f30c9921162dc0c2975482f69072
xorh02 <xavierong02@gmail.com>
2026-08-30 09:22:17 +0000

    Aug 30, 2026, 9:22 AM

--- revision.js
+++ revision.js
@@ -38,7 +38,8 @@ must have functions in p5js -> setup() and draw()
 in for loop be very careful of the initial variable declaration
 for loop syntax -> for(let i =0; i<5;i++)
 for loop syntax -> for(declaration; condition; increment)
-to 
+to create new canvas -> createCanvas(width,height)
+
 
 
 */
