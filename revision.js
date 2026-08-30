cb0386b46b98d63649b3ede6182bdf8e066945bf
xorh02 <xavierong02@gmail.com>
2026-08-30 09:05:57 +0000

    Aug 30, 2026, 9:05 AM

--- revision.js
+++ revision.js
@@ -2,6 +2,7 @@ function setup(){
     createCanvas(600,400);
     background(0); //0 is black 255 is white
     fill(0);
-    stroke(255,0,0);
+    stroke(255,0,0);//change outline color
+    //
 
 }
