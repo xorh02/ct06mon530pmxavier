a865439e1ae84e1be34a3e7b0d35dbbff77915b6
xorh02 <xavierong02@gmail.com>
2026-08-30 09:06:33 +0000

    Aug 30, 2026, 9:06 AM

--- revision.js
+++ revision.js
@@ -4,6 +4,8 @@ function setup(){
     fill(0);
     stroke(255,0,0);//change outline color
     //triangle 6 parameters
-    triangle(260,240,300,160,340,240)
+    triangle(260,240,300,160,340,240);
+    //circle 3 parameter x,y,d
+    circle()
 
 }
